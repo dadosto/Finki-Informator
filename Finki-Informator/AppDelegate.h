757636d6a92a4b46c1eaebd47e2a595dc5be8dd8
Vladimir <dadosto@gmail.com>
2013-05-21 23:05:30 +0200

    Initial Commit

---
+++ Finki-Informator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Finki-Informator
+//
+//  Created by Dado on 5/21/13.
+//  Copyright (c) 2013 Finki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
